1fdc6dccfda6f921e606715d8e258402383703d2
Juergen <jurgan.kaltan@gmx.at>
2022-05-12 15:34:38 +0200

    add something stupid

--- middle.sh
+++ middle.sh
@@ -2,10 +2,11 @@
 # action : take the lines between number 10 and 15 
 # output : show the results on screen 
 
-for filename in  ls p*
+for filename in "$@"
 do
 echo $filename
 head -n 15 $filename | tail -n 5 
+something stupid
 echo
 done
 
