ed0732ac79efdc49fda4a1ad09728828618cbbe3
Juergen <jurgan.kaltan@gmx.at>
2022-05-12 14:22:44 +0200

    Add comment

--- middle.sh
+++ middle.sh
@@ -1,3 +1,7 @@
+# inpud: list of text files 
+# action : take the lines between number 10 and 15 
+# output : show the results on screen 
+
 for filename in  ls p*
 do
 head -n 15 $filename | tail -n 5 
